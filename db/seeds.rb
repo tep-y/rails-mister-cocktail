e2ef631e7ca339dbbe125c89fb55cab8c0d1325a
tep yith <tep.yith@gmail.com>
2021-02-20 14:16:04 -0500

    Completed Cocktail controller.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.delete_all
+puts "Deleting current Database.."
+
+puts "Creating Database.."
+
+Ingredient.create(name: "Tequila")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Rum")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Orange Liquor")
+
+Ingredient.create(name: "Lime")
+Ingredient.create(name: "Lemon")
+Ingredient.create(name: "Sugar")
+Ingredient.create(name: "Mint leaves")
+Ingredient.create(name: "Orange Juice")
+
+puts "Done!"
