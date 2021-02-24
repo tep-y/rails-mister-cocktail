35b8048e144407a00a034a27dd7f36ad2a7458d6
tep yith <tep.yith@gmail.com>
2021-02-23 19:29:44 -0500

    Added links to pages. Able to create new cocktails, and add doses.

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,9 @@ Rails.application.routes.draw do
   # get '/cocktails/new', to: 'cocktails#new'
   # post '/cocktails', to: 'cocktails#create'
 
-  resources :cocktails, only: [:index, :show, :new, :create]
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: [:destroy]
 end
