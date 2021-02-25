86e982e34621bc30adfafeffce6c52e6a7e60e5a
tep yith <tep.yith@gmail.com>
2021-02-25 15:44:13 -0500

    Added some frontend to Landing Page, and created root to index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  ## COCKTAIL ROUTES
-  ## A user can see the list of cocktails
-  # get '/cocktails', to: 'cocktails#index'
-
-  ## A user can see the details of a given cocktail, with the dose needed for each ingredient
-  # get '/cocktails/:id', to: 'cocktails#show'
-
-  ## A user can create a new cocktail
-  # get '/cocktails/new', to: 'cocktails#new'
-  # post '/cocktails', to: 'cocktails#create'
-
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
 
