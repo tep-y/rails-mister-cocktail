faf2bf48593d34c150fdcb24b29c3738345bd7e2
tep yith <tep.yith@gmail.com>
2021-02-25 20:54:18 -0500

    Added user upload image button when creating a new cocktail.

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,8 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_one_attached :photo
 
+  validates :photo, presence: true
   validates :name, presence: true, uniqueness: true
 end
